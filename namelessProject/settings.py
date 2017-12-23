b5fc36c2b67e1c60641e19834034618ff713e4c6
Kenan7 <kenankzmv@gmail.com>
2017-12-24 01:29:38 +0300

    moving static folder

--- namelessProject/settings.py
+++ namelessProject/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'namelessProject/static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+kv)berxg%7wef$n60gqs+^@uz2^o842t!c()5hp&td=%+cvdu'
