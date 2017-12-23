8d78cde57ba8d6d18d97a270ef610b078a04aec8
Kenan7 <kenankzmv@gmail.com>
2017-12-24 01:37:39 +0300

    moving static folder

--- namelessProject/settings.py
+++ namelessProject/settings.py
@@ -2,7 +2,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, 'namelessProject/static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+kv)berxg%7wef$n60gqs+^@uz2^o842t!c()5hp&td=%+cvdu'
@@ -90,7 +89,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
